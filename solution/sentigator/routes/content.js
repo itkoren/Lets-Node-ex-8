4c8f8e5ca02da378645bc6e722170fac2f1ed801
Itai Koren <itkoren@gmail.com>
2014-05-24 10:48:43 +0300

    Exercise Solution Initial Commit
    Signed-off-by: Itai Koren <itkoren@gmail.com>

---
+++ solution/sentigator/routes/content.js
@@ -0,0 +1,27 @@
+// Include The 'express' Module
+var express = require("express");
+
+// Include The 'aggregator' Module
+var aggregator = require("../lib/aggregator");
+
+// Initialize a new express router
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    // Handle HTTP Request
+    var qs = req.query;
+
+    aggregator(qs.term, function(err, data) {
+        if (err) {
+            // Deal with errors
+            console.log("Got error: " + err.message);
+            res.writeHead(500);
+            res.end(err.message);
+        }
+        else {
+            res.json(data);
+        }
+    });
+});
+
+module.exports = router;
